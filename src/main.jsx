be64290f2e401cc7c3b108a3e505a4784bc9cd1a
Shanu <55043587+Shanu-web@users.noreply.github.com>
2025-09-20 18:41:38 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,14 @@ import { LocaleProvider } from './context/LocaleContext';
 import { ThemeProvider } from './context/ThemeContext';
 import './index.css';
 
+const baseName = (import.meta.env.BASE_URL ?? '/').replace(/\/$/, '');
+const routerBaseName = baseName === '' || baseName === '/' ? undefined : baseName;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <LocaleProvider>
-        <BrowserRouter basename="/shanu-portfolio">
+        <BrowserRouter basename={routerBaseName}>
           <App />
         </BrowserRouter>
       </LocaleProvider>
