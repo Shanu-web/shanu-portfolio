4288ba61b62248a6dcf9a4bf297032408c0211c4
Shanu <55043587+Shanu-web@users.noreply.github.com>
2025-09-20 18:40:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Projects from './pages/Projects';
 const App = () => {
   return (
     <Routes>
-      <Route element={<Layout />}>
+      <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="projects" element={<Projects />} />
